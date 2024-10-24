5b674b54f99ee33f8ca095c5060252d426cb36ab
laiscrz <lais.albino2003@gmail.com>
2024-10-24 15:15:45 -0300

    feat: Initialize Spring Boot project

---
+++ src/test/java/br/com/app/VisitMap/VisitMapApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.app.VisitMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
