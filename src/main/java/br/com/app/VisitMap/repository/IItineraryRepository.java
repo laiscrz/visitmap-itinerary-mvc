c3540add3af302daa9e667293e5474b9c7e655d4
laiscrz <lais.albino2003@gmail.com>
2024-10-24 17:55:38 -0300

    feat: created pages

--- src/main/java/br/com/app/VisitMap/repository/IItineraryRepository.java
+++ src/main/java/br/com/app/VisitMap/repository/IItineraryRepository.java
@@ -1,8 +1,9 @@
 package br.com.app.VisitMap.repository;
 
+import br.com.app.VisitMap.model.Itinerary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IItineraryRepository extends JpaRepository<Long, IItineraryRepository> {
+public interface IItineraryRepository extends JpaRepository<Itinerary, Long> {
 }
