525e24163fdb6ddcd3afeab39c8b72f79e6891e2
laiscrz <lais.albino2003@gmail.com>
2024-10-24 23:53:01 -0300

    feat: created about details place

--- src/main/java/br/com/app/VisitMap/controller/PlaceController.java
+++ src/main/java/br/com/app/VisitMap/controller/PlaceController.java
@@ -1,10 +1,14 @@
 package br.com.app.VisitMap.controller;
 
 import br.com.app.VisitMap.model.Place;
+import br.com.app.VisitMap.model.enums.TypeEntry;
+import br.com.app.VisitMap.model.enums.TypePlace;
 import br.com.app.VisitMap.service.PlaceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,11 +22,30 @@ public class PlaceController {
     private PlaceService placeService;
 
     @GetMapping
-    public ModelAndView listPlaces() {
+    public ModelAndView showListPlaces() {
         List<Place> musics = placeService.findAllPlaces();
         ModelAndView modelAndView = new ModelAndView("place/list");
         modelAndView.addObject("places", musics);
         return modelAndView;
     }
+
+    @GetMapping("/{id}")
+    public ModelAndView showMusicDetails(@PathVariable Long id) {
+        Place music = placeService.findByIdPlace(id);
+        ModelAndView modelAndView = new ModelAndView("place/details");
+        modelAndView.addObject("place", music);
+        return modelAndView;
+    }
+
+    @ModelAttribute("tipos")
+    public TypePlace[] getTipos(){
+        return TypePlace.values();
+    }
+
+    @ModelAttribute("entrada")
+    public TypeEntry[] getEntrada(){
+        return TypeEntry.values();
+    }
+
 
 }
