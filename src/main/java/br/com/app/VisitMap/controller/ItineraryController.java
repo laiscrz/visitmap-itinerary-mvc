a8bac9e6cd111ff45083e7508c4fbcfeb1881cb4
laiscrz <lais.albino2003@gmail.com>
2024-10-25 20:58:28 -0300

    feat: implemented method to delete itinerary

--- src/main/java/br/com/app/VisitMap/controller/ItineraryController.java
+++ src/main/java/br/com/app/VisitMap/controller/ItineraryController.java
@@ -1,11 +1,14 @@
 package br.com.app.VisitMap.controller;
 
 import br.com.app.VisitMap.model.Itinerary;
+import br.com.app.VisitMap.model.enums.TypeCountry;
+import br.com.app.VisitMap.model.enums.TypeEntry;
 import br.com.app.VisitMap.service.ItineraryService;
 import br.com.app.VisitMap.service.PlaceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,7 +26,7 @@ public class ItineraryController {
     private PlaceService placeService;
 
     @GetMapping
-    public ModelAndView listPlaylists() {
+    public ModelAndView listItineraries() {
         List<Itinerary> itineraries = itineraryService.findAllItneraries();
         ModelAndView modelAndView = new ModelAndView("itinerary/list");
         modelAndView.addObject("itineraries", itineraries);
@@ -31,10 +34,28 @@ public class ItineraryController {
     }
 
     @GetMapping("/{id}")
-    public ModelAndView showPlaylistDetails(@PathVariable Long id) {
+    public ModelAndView showItineraryDetails(@PathVariable Long id) {
         Itinerary itinerary = itineraryService.findByIdItinerary(id);
         ModelAndView modelAndView = new ModelAndView("itinerary/details");
         modelAndView.addObject("itinerary", itinerary);
         return modelAndView;
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView deleteItinerary(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("itinerary/list");
+        try {
+            itineraryService.deleteByIdItinerary(id);
+            modelAndView.addObject("message", "Playlist excluída com sucesso.");
+        } catch (RuntimeException e) {
+            modelAndView.addObject("error", e.getMessage());
+        }
+        modelAndView.addObject("itineraries", itineraryService.findAllItneraries());
+        return modelAndView;
+    }
+
+    @ModelAttribute("pais")
+    public TypeCountry[] getPais(){
+        return TypeCountry.values();
     }
 }
