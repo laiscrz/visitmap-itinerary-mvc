bef4240a0193a9cba022d05767941004e456f31d
laiscrz <lais.albino2003@gmail.com>
2024-10-25 21:35:46 -0300

    feat: updated import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
-INSERT INTO itinerary (nome, pais, data_criacao) VALUES ('Exploração do Brasil', 'BRASIL', '2024-10-24');
-INSERT INTO itinerary (nome, pais, data_criacao) VALUES ('Tour pela Europa', 'FRANCA', '2024-10-24');
+INSERT INTO itinerary (nome, pais, data_criacao) VALUES ('Exploração do Brasil', 'BRASIL', TO_DATE('2024-10-24', 'YYYY-MM-DD'));
+INSERT INTO itinerary (nome, pais, data_criacao) VALUES ('Tour pela Europa', 'FRANCA', TO_DATE('2024-10-24', 'YYYY-MM-DD'));
 
-INSERT INTO place (nome, descricao, cidade, estado, rua, tipo, entrada, url_imagem) VALUES ('Cristo Redentor', 'Estátua icônica no Rio de Janeiro', 'Rio de Janeiro', 'RJ', 'Rua Cosme Velho', 'Estatua', 'PAGO', 'https://abrir.link/ZLXgi');
+INSERT INTO place (nome, descricao, cidade, estado, rua, tipo, entrada, url_imagem) VALUES ('Cristo Redentor', 'Estátua icônica no Rio de Janeiro', 'Rio de Janeiro', 'RJ', 'Rua Cosme Velho', 'ESTATUA', 'PAGO', 'https://abrir.link/ZLXgi');
 INSERT INTO place (nome, descricao, cidade, estado, rua, tipo, entrada, url_imagem) VALUES ('Museu do Ipiranga', 'Museu histórico localizado em São Paulo', 'São Paulo', 'SP', 'Parque da Independência', 'MUSEU', 'PAGO', 'https://abrir.link/rztKU');
 INSERT INTO place (nome, descricao, cidade, estado, rua, tipo, entrada, url_imagem) VALUES ('Torre Eiffel', 'Famoso marco em Paris', 'Paris', 'Ile-de-France', 'Champ de Mars', 'MONUMENTO', 'PAGO', 'https://abrir.link/NdfEB');
 INSERT INTO place (nome, descricao, cidade, estado, rua, tipo, entrada, url_imagem) VALUES ('Museu do Louvre', 'Museu de arte famoso em Paris', 'Paris', 'Ile-de-France', 'Rue de Rivoli', 'MUSEU', 'PAGO', 'https://abrir.link/fYLhP');
